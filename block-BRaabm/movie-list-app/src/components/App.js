b5dba328e096607b4b0f93cbda8f9f7392f21d19
Kunal <mailto.guptakunal@gmail.com>
2023-01-21 14:56:14 +0530

    create moviewatchlist app with react

---
+++ block-BRaabm/movie-list-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import data from 'data.json';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: false,
+      name: '',
+    };
+  }
+
+  render (){
+    let movieDetails;
+    if (this.state.name !== "")
+  }
+}
+export default App;
---
+++ block-BRaabm/movie-list-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './Component/App';
+import './styles.css';
+
+let rootElement = document.getElementById('root');
+
+ReactDOM.createRoot(rootElement).render(<App />);
